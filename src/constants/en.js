619f037e8834423e3f48ff432c28ced92978b8ab
Matthew Kitt <mk@mkitt.net>
2018-04-23 12:58:06 -0600

    Add meta and open graph tags to the head
    Includes the bare minimum for favicon and touch icons

---
+++ src/constants/en.js
@@ -0,0 +1,12 @@
+// @flow
+
+export default undefined
+
+export const META = Object.freeze({
+  DESCRIPTION: 'trvrsl description',
+  ICON: '/favicon.svg',
+  IMAGE: '/apple-touch-icon.png',
+  NAME: 'trvrsl name',
+  TITLE: 'trvrsl title',
+  URL: 'https://turingschool.github.io/trvrsl/',
+})
--- src/templates/Document.js
+++ src/templates/Document.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { type ElementType, type Node } from 'react'
-import { GLOBAL_CSS } from '../constants/styles'
+import { META } from '../constants/en'
+import { COLOR, GLOBAL_CSS } from '../constants/styles'
 
 export type RenderMeta = {
   css: string,
@@ -25,8 +26,26 @@ export default class extends React.Component<Props> {
     return (
       <Html>
         <Head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <title>{META.TITLE}</title>
+          <meta name="application-name" content={META.NAME} />
+          <meta name="subject" content={META.DESCRIPTION} />
+          <meta name="name" itemProp="name" content={META.NAME} />
+          <meta name="image" itemProp="image" content={META.IMAGE} />
+          <meta name="url" itemProp="url" content={META.URL} />
+          <meta name="description" itemProp="description" content={META.DESCRIPTION} />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content={META.NAME} />
+          <meta property="og:title" content={META.TITLE} />
+          <meta property="og:image" content={META.IMAGE} />
+          <meta property="og:url" content={META.URL} />
+          <meta property="og:description" content={META.DESCRIPTION} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="referrer" content="always" />
+          <meta name="robots" content="index, follow" />
+          <link href={META.ICON} color={COLOR} rel="mask-icon" />
           <link href="https://fonts.googleapis.com/css?family=Barlow:200,300,400,700" rel="stylesheet" />
           { /* eslint-disable react/no-danger */ }
           <style dangerouslySetInnerHTML={{ __html: GLOBAL_CSS + renderMeta.css }} />
@@ -40,3 +59,4 @@ export default class extends React.Component<Props> {
     )
   }
 }
+
