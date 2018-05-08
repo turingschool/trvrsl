189e6f6bf915177a90427b736d07cca2344c0988
Jeff Casimir <jeff@casimircreative.com>
2018-05-08 14:12:44 -0600

    Beef up metadata

--- src/constants/en.js
+++ src/constants/en.js
@@ -12,12 +12,12 @@ export const FORMS = Object.freeze({
 })
 
 export const META = Object.freeze({
-  DESCRIPTION: 'Traversal (Sept. 28-29, 2018) is a two-day conference bringing together technologists from many disciplines to share ideas.',
+  DESCRIPTION: 'Traversal is a two-day conference bringing together technologists from many disciplines to share ideas.',
   FAVICON: '/favicon.ico',
   ICON: '/favicon.svg',
   IMAGE: '/apple-touch-icon.png',
   MANIFEST: '/manifest.json',
-  NAME: 'trvrsl name',
-  TITLE: 'Traversal 2018',
+  NAME: 'Traversal',
+  TITLE: 'Traversal 2018: September 27 & 28 in Denver, CO',
   URL: 'https://traversalconf.com',
 })
